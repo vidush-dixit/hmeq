c4aaef25dfaa13fe6e22d761d9f7c4e4c0754957
Vidush Dixit <32012141+vidush-dixit@users.noreply.github.com>
2020-06-20 12:31:33 +0530

    Initial

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ def analyse():
         img_buffer.seek(0)
         plot_url = base64.b64encode( img_buffer.getvalue() ).decode('utf-8')
 
-        if( temp_result ):
+        if( temp_result == 0 ):
             category = 'GOOD'
             resType = 'success'
         else:
